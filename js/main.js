89edeed93bb75a1ccb34c3c03dc418235e9f78a1
Andrey <biondohod@gmail.com>
2022-07-16 17:16:22 +0200

    Local Storage

--- js/main.js
+++ js/main.js
@@ -471,12 +471,44 @@ document.addEventListener('DOMContentLoaded', () => {
 
     //Калькулятор калорий
     const calories = document.querySelector('.calculating__result span');
-    let sex = 'female',
-        height, weight, age,
-        ratio = 1.375;
+    if (!localStorage.getItem('sex')) {
+        localStorage.setItem('sex', 'female');
+    }
+    if (!localStorage.getItem('ratio')) {
+        localStorage.setItem('ratio', 1.375);
+    }
+    const setLocalStorageData = () => {
+        // Set sex
+        switch(localStorage.getItem('sex')) {
+            case 'male':
+                document.querySelector('#female').classList.remove('calculating__choose-item_active');
+                document.querySelector('#male').classList.add('calculating__choose-item_active');
+                break;
+        };
+        // Set ratio
+        const ratioElements = document.querySelectorAll('.calculating__choose_big .calculating__choose-item');
+        const ratio = localStorage.getItem('ratio');
+        ratioElements.forEach((element) => {
+            if (element.getAttribute('data-ratio') === ratio) {
+                element.classList.add('calculating__choose-item_active');
+            } else {
+                element.classList.remove('calculating__choose-item_active');
+            }
+        });
+        // Set inputs
+        document.querySelector('#height').value = localStorage.getItem('height');
+        document.querySelector('#weight').value = localStorage.getItem('weight');
+        document.querySelector('#age').value = localStorage.getItem('age');
+    };
+    setLocalStorageData();
+
     const calcCalories = () => {
+        const sex = localStorage.getItem('sex');
+        const height = +localStorage.getItem('height');
+        const weight = +localStorage.getItem('weight');
+        const age = +localStorage.getItem('age');
+        const ratio = +localStorage.getItem('ratio');
         if (!sex || !height || !weight || !age || !ratio) {
-            console.log(sex, height, ratio);
             calories.textContent = '____';
             return;
         }
@@ -493,9 +525,11 @@ document.addEventListener('DOMContentLoaded', () => {
         elements.forEach((element) => {
             element.addEventListener('click', (evt) => {
                 if (evt.target.getAttribute('data-ratio')) {
-                    ratio = +evt.target.getAttribute('data-ratio');
+                    localStorage.setItem('ratio', +evt.target.getAttribute('data-ratio'));
+                    // ratio = +evt.target.getAttribute('data-ratio');
                 } else {
-                    sex = evt.target.getAttribute('id');
+                    localStorage.setItem('sex', evt.target.getAttribute('id'));
+                    // sex = evt.target.getAttribute('id');
                 }
                 elements.forEach((item) => {
                     item.classList.remove(activeClass);
@@ -512,15 +546,23 @@ document.addEventListener('DOMContentLoaded', () => {
         const elements = document.querySelectorAll(`${selector} input`);
         elements.forEach((element) => {
             element.addEventListener('input', (evt) => {
+                if (element.value.match(/\D/g)) {
+                    element.style.outline = '1px solid red';
+                } else {
+                    element.style = '';
+                }
                 switch(element.id) {
                     case 'height':
-                        height = +evt.target.value;
+                        // height = +evt.target.value;
+                        localStorage.setItem('height', +evt.target.value);
                         break;
                     case 'weight':
-                        weight = +evt.target.value;
+                        // weight = +evt.target.value;
+                        localStorage.setItem('weight', +evt.target.value);
                         break;
                     case 'age':
-                        age = +evt.target.value;
+                        // age = +evt.target.value;
+                        localStorage.setItem('age', +evt.target.value);
                         break;
                 };
                 calcCalories();
