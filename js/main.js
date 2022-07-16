39842ddb37dfdf51fda74d555daffe87bfdb832b
Andrey <biondohod@gmail.com>
2022-07-16 14:59:01 +0200

    Навигация для слайдера

--- js/main.js
+++ js/main.js
@@ -358,22 +358,25 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Slider with animation 
     let width = window.getComputedStyle(sliderWrapper).width;
-    width = +width.slice(0, width.length - 2);
+    width = +width.replace(/\D/g, '');
 
     sliderField.style.width = `${100 * sliderPhotos.length}%`;
     sliderField.style.display = 'flex';
     sliderField.style.transition = '0.8s all';
     sliderWrapper.style.overflow = 'hidden';
     sliderPhotos.forEach(slide => slide.style.width = `${width}px`);
+    sliderTotal.textContent = getZero(sliderPhotos.length);
     sliderCurrent.textContent = getZero(1);
     let offset = 0;
     const togglePrevSlide = () => {
         if (offset === 0) {
             offset = width * (sliderPhotos.length -1);
             sliderCurrent.textContent = getZero(sliderPhotos.length);
+            setCurrentDot();
         } else {
             offset -= width;
             sliderCurrent.textContent = getZero(sliderCurrent.textContent - 1);
+            setCurrentDot();
         }
         sliderField.style.transform = `translateX(-${offset}px)`;
     };
@@ -381,9 +384,11 @@ document.addEventListener('DOMContentLoaded', () => {
         if (offset === width * (sliderPhotos.length -1)) {
             offset = 0;
             sliderCurrent.textContent = getZero(1);
+            setCurrentDot();
         } else {
             offset += width;
             sliderCurrent.textContent = getZero(+sliderCurrent.textContent + 1);
+            setCurrentDot();
         }
         sliderField.style.transform = `translateX(-${offset}px)`;
     };
@@ -422,4 +427,45 @@ document.addEventListener('DOMContentLoaded', () => {
     // sliderCurrent.textContent = getZero(1);
     // sliderPrevious.addEventListener('click', togglePrevSlide);
     // sliderNext.addEventListener('click', toggleNextSlide);
+
+    // Точки для слайдера 
+    const sliderNavigation = document.createElement('ol');
+    const createSliderNavigation = () => {
+        sliderWrapper.style.position = 'relative';
+        sliderNavigation.classList.add('carousel-indicators');
+        for (let i = 0; i < sliderTotal.textContent; i++) {
+            const dot = document.createElement('li');
+            dot.classList.add('dot');
+            dot.setAttribute('data-slide-to', i+1);
+            if (i+1 == sliderCurrent.textContent) {
+                dot.style.opacity = 1;
+            }
+            sliderNavigation.append(dot);
+        };
+        sliderWrapper.append(sliderNavigation);
+    };
+    createSliderNavigation();
+    const dotsButton = sliderNavigation.querySelectorAll('li');
+    const setSlideByDot = (dot) => {
+        const dotData = dot.dataset.slideTo;
+        offset = width * (dotData -1);
+        sliderCurrent.textContent = getZero(dotData);
+        sliderField.style.transform = `translateX(-${offset}px)`;
+        setCurrentDot();
+    };
+
+    dotsButton.forEach((dot) => {
+        dot.addEventListener('click', (evt) => setSlideByDot(evt.target));
+    });
+
+    function setCurrentDot() {
+        const index = (offset / width) + 1;
+        dotsButton.forEach((dot) => {
+            if (index == dot.dataset.slideTo) {
+                dot.style.opacity = 1;
+            } else {
+                dot.style = '';
+            }
+        });
+    }
 });
